315c466049ffc7eb440ccd65164a93623356b156
Justin <justin.zy.wang@gmail.com>
2018-08-12 04:23:21 +0800

    add PWM

--- install-ldap.sh
+++ install-ldap.sh
@@ -10,7 +10,9 @@ currentTime()
 }
 
 sudo docker service scale devops-ldap=0
+sudo docker service scale devops-ldap-pwmdb=0
 sudo docker service scale devops-ldapui=0
+sudo docker service scale devops-ldap-pwm=0
 
 echo ---$(currentTime)---populate the volumes---
 #to zip, use: sudo tar zcvf devops_ldap_volume.tar.gz /var/nfs/volumes/devops_ldap*
@@ -29,6 +31,16 @@ volume-driver=local-persist,volume-opt=mountpoint=/var/nfs/volumes/devops_ldap_v
 --constraint 'node.role == manager' \
 $LDAP_IMAGE
 
+echo ---$(currentTime)---create ldap pwmdb service---
+sudo docker service create -d \
+--name devops-ldap-pwmdb \
+--mount type=volume,source=devops_ldap_pwmdb_volume,destination=/var/lib/mysql,\
+volume-driver=local-persist,volume-opt=mountpoint=/var/nfs/volumes/devops_ldap_pwmdb_volume \
+--network $NETWORK_NAME \
+--replicas 1 \
+--constraint 'node.role == manager' \
+$LDAP_PWMDB_IMAGE
+
 echo ---$(currentTime)---create ldap ui service---
 sudo docker service create -d \
 --publish $LDAP_PORT:80 \
@@ -38,6 +50,19 @@ sudo docker service create -d \
 --constraint 'node.role == manager' \
 $LDAPUI_IMAGE
 
+echo ---$(currentTime)---create ldap pwm service---
+sudo docker service create -d \
+--publish $LDAP_PWM_PORT:8443 \
+--name devops-ldap-pwm \
+--mount type=volume,source=devops_ldap_pwm_volume,destination=/config,\
+volume-driver=local-persist,volume-opt=mountpoint=/var/nfs/volumes/devops_ldap_pwm_volume \
+--network $NETWORK_NAME \
+--replicas 1 \
+--constraint 'node.role == manager' \
+$LDAP_PWM_IMAGE
+
+sudo docker service scale devops-ldap-pwm=1
 sudo docker service scale devops-ldapui=1
+sudo docker service scale devops-ldap-pwmdb=1
 sudo docker service scale devops-ldap=1
 
